cc8191788b33269f4c5e10e38312227b49c992dd
Moira Leon <moira.leon2@gmail.com>
2021-10-20 19:17:58 -0600

    working out bugs

--- src/com/javapackage/Main.java
+++ src/com/javapackage/Main.java
@@ -1,5 +1,6 @@
 package com.javapackage;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -9,21 +10,46 @@ public class Main {
         Scanner principleScanner = new Scanner(System.in);
         System.out.print("Principle: ");
         int principleInput = principleScanner.nextInt();
-        System.out.println("Your Principle is "+ principleInput);
+       // System.out.println("Your Principle is "+ principleInput);
 
         //get annual interest rate
         Scanner AIRScanner = new Scanner(System.in);
         System.out.print("Annual Interest Rate: ");
         float AIRInput = AIRScanner.nextFloat();
-        System.out.println("Your Annual Interest Rate is "+ AIRInput);
+       // System.out.println("Your Annual Interest Rate is "+ AIRInput);
 
         //get period in years
         Scanner periodScanner = new Scanner(System.in);
         System.out.print("Period (Years): ");
         int periodInput = periodScanner.nextInt();
-        System.out.println("Your Period in Years is "+ periodInput);
+       // System.out.println("Your Period in Years is "+ periodInput);
 
 
+        //print Mortgage
+        //formula for mortgage = principle * monthly interest rate(annual interest rate/100(to make it a percentage) then /12)
+        //n = number of payments years period x 12
+        //raising to power requires Math.pow
+
+        //get all values
+
+        float r = (AIRInput/100)/12;
+        //System.out.println(r);
+        double n = periodInput * 12;
+        double top = Math.pow(r*(1+r),n);
+        System.out.println(top);
+        double bottom = (Math.pow((1+r),n))-1;
+        System.out.println(bottom);
+
+        //System.out.println(n);
+
+        //formula for mortgage result
+        double mortgageResult = principleInput *(top/bottom);
+        System.out.println(mortgageResult);
+
+        //convert input to currency input
+        String mortgagePayment = NumberFormat.getCurrencyInstance().format(mortgageResult);
+
+        System.out.println("Mortgage:" + mortgagePayment);
 
     }
 }
