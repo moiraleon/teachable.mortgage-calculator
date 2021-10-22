46dfb83083fd8885d3c1b108176a04f63a1cd16e
Moira Leon <moira.leon2@gmail.com>
2021-10-22 17:14:51 -0600

    refactoring

--- src/com/javapackage/Main.java
+++ src/com/javapackage/Main.java
@@ -5,4 +5,27 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static double readNumber(String prompt, double min, double max){
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextInt();
+            if (value >= min && value <= max)
+                break;
+            else {
+                System.out.println("Please enter a a value between "+min+ " and "+max);
+            }
+        }
+        return calculateMortgage();
+    };
+
+    public static double calculateMortgage(
+            int principleInput,
+            float AIRInput,
+            int periodInput){
+
+    }
+
+
 }
